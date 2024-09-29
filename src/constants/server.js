1b202b85ab768e703a813986799f5c2c07b1528e
ishan himalka <64436393+ishanhimalka@users.noreply.github.com>
2024-09-29 20:20:23 +0200

    Update server.js

--- src/constants/server.js
+++ src/constants/server.js
@@ -1,6 +1,6 @@
 export default {
     // API_URL: 'http://5.189.141.126:5000',
-    API_URL: "https://5.189.141.126:443"
+    API_URL: "https://109.199.102.246:443"
     // API_URL: "http://127.0.0.1:5000",
 
 }
